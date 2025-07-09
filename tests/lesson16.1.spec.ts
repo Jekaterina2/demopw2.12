f942c6d2f88e3cc9cd6705ff0a7b11e46cc8e257
37255 <aleksandra.kulakova.ndu@gmail.com>
2025-07-09 20:45:45 +0300

    Added .env file to gitignore

--- tests/lesson16.1.spec.ts
+++ tests/lesson16.1.spec.ts
@@ -1,6 +1,10 @@
 import {expect, Locator, test} from "@playwright/test";
+import { faker } from "@faker-js/faker/locale/ar";
 import * as dotenv from 'dotenv';
 dotenv.config();
+
+const randomUsername = faker.internet.username();
+const randomPassword = faker.internet.password();
 
 test.beforeEach(async ({ page }) => {
     await page.goto(process.env.APP_URL!);
@@ -15,5 +19,14 @@ test('sign-in button is disabled', async ({ page }) => {
     await password.fill('test');
     await expect(loginButton).toBeDisabled();
 
+});
+
+test('By using random username and random password, username and password fields are defined', async ({ page }) => {
+    const usernameInput: Locator = page.locator('#username');
+    const passwordInput: Locator = page.locator('#password');
+    await usernameInput.fill(randomUsername);
+    await passwordInput.fill(randomPassword);
+    expect(usernameInput).toBeDefined();
+    expect(passwordInput).toBeDefined();
 });
 
